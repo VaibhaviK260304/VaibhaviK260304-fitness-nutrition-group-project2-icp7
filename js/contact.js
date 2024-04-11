2025ad394280806bf10c1b4d454adc67837a12e7
sakshigore2153 <sakshigore2153@gmail.com>
2024-04-11 15:38:28 +0530

    add js file code

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,27 @@
 // submit
 
-function submit()
+function submit(){
+    let name = document.getElementById("name");
+    let number = document.getElementById("number");
+
+    if(name.value == "")
+    {
+        alert("Please Enter Your Name")
+    }
+    else 
+    if(number.value == "")
+    {
+        alert("Please Enter Your Number")
+    }
+    else 
+    if(email.value == "")
+    {
+        alert("Please Enter Your Email ID")
+    }
+
+    else
+    {
+        alert("Thanks for Join : " + name.value)
+    }
+
+}
