ce8e1fe42d19895fcfac7dd3bef6eea00832ef48
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-04-12 16:11:51 +0530

    added js

--- js/gym_facilities.js
+++ js/gym_facilities.js
@@ -11,3 +11,22 @@ function showRoutine(){
             }, 1000);
 
 }
+let previousCard;
+
+function changeColor(card) {
+    if (previousCard) {
+        previousCard.style.backgroundColor = '#ffff';
+        previousCard.querySelector('.card-body').style.backgroundColor = '#ffff';
+        previousCard.querySelector('.get-btn').style.color = 'white';
+        previousCard.querySelector('.get-btn').classList.add('bg-dark');
+        previousCard.querySelector('.price').style.color = '#fff';
+    }
+    card.style.transition = "background-color.4s";
+    card.style.backgroundColor = "#ffc36b";
+    card.querySelector('.card-body').style.backgroundColor = "#ffc36b";
+    card.querySelector('.price').style.color = 'red';
+    card.querySelector('.get-btn').classList.remove('bg-dark') ;
+    card.querySelector('.get-btn').style.backgroundColor = 'gold';
+    card.querySelector('.get-btn').style.color = 'black';
+    previousCard = card;
+}
