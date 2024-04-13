02690d049eb252bafd7d73845b053bb6f99754b6
sakshigore2153 <sakshigore2153@gmail.com>
2024-04-13 13:11:13 +0530

    changes in style.js

--- js/style.js
+++ js/style.js
@@ -1,54 +1,70 @@
 /*Nav Bar Js*/
+
+function index(){
+    document.getElementById("index").style.color="rgb(20,253,30)";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("guide").style.color="white";
+    document.getElementById("feedback").style.color="white";
+    document.getElementById("login").style.color="white";
+    document.getElementById("registration").style.color="white";
+}
 function about(){
+    document.getElementById("index").style.color="white";
     document.getElementById("about").style.color="rgb(20,253,30)";
-    document.getElementById("contact").style.color="black";
-    document.getElementById("guide").style.color="black";
-    document.getElementById("feedback").style.color="black";
-    document.getElementById("login").style.color="black";
-    document.getElementById("registration").style.color="black";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("guide").style.color="white";
+    document.getElementById("feedback").style.color="white";
+    document.getElementById("login").style.color="white";
+    document.getElementById("registration").style.color="white";
 }
 function contact(){
-    document.getElementById("about").style.color="black";
+    document.getElementById("index").style.color="white";
+    document.getElementById("about").style.color="white";
     document.getElementById("contact").style.color="rgb(20,253,30)";
-    document.getElementById("guide").style.color="black";
-    document.getElementById("feedback").style.color="black";
-    document.getElementById("login").style.color="black";
-    document.getElementById("registration").style.color="black";
+    document.getElementById("guide").style.color="white";
+    document.getElementById("feedback").style.color="white";
+    document.getElementById("login").style.color="white";
+    document.getElementById("registration").style.color="white";
 
 }
 function guide(){
-    document.getElementById("about").style.color="black";
-    document.getElementById("contact").style.color="black";
+    document.getElementById("index").style.color="white";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
     document.getElementById("guide").style.color="rgb(20,253,30)";
-    document.getElementById("feedback").style.color="black";
-    document.getElementById("login").style.color="black";
-    document.getElementById("registration").style.color="black";
+    document.getElementById("feedback").style.color="white";
+    document.getElementById("login").style.color="white";
+    document.getElementById("registration").style.color="white";
 
 }
 function feedback(){
-    document.getElementById("about").style.color="black";
-    document.getElementById("contact").style.color="black";
-    document.getElementById("guide").style.color="black";
+    document.getElementById("index").style.color="white";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("guide").style.color="white";
     document.getElementById("feedback").style.color="rgb(20,253,30)";
-    document.getElementById("login").style.color="black";
-    document.getElementById("registration").style.color="black";
+    document.getElementById("login").style.color="white";
+    document.getElementById("registration").style.color="white";
 
 }
 function login(){
-    document.getElementById("about").style.color="black";
-    document.getElementById("contact").style.color="black";
-    document.getElementById("guide").style.color="black";
-    document.getElementById("feedback").style.color="black";
+    document.getElementById("index").style.color="white";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("guide").style.color="white";
+    document.getElementById("feedback").style.color="white";
     document.getElementById("login").style.color="rgb(20,253,30)";
     document.getElementById("registration").style.color="white";
 
 }
 function registration(){
-    document.getElementById("about").style.color="black";
-    document.getElementById("contact").style.color="black";
-    document.getElementById("guide").style.color="black";
-    document.getElementById("feedback").style.color="black";
-    document.getElementById("login").style.color="black";
+    document.getElementById("index").style.color="white";
+    document.getElementById("about").style.color="white";
+    document.getElementById("contact").style.color="white";
+    document.getElementById("guide").style.color="white";
+    document.getElementById("feedback").style.color="white";
+    document.getElementById("login").style.color="white";
     document.getElementById("registration").style.color="rgb(20,253,30)";
 
 }
