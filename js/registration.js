0994ca965d3e2797e3f4e710a1189d936ee274c5
rutujamohite916 <rutujamohite916@gmail.com>
2024-04-13 14:51:11 +0530

    registation code changes

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,41 @@
+
+function data(){
+
+
+var a=document.getElementById("n1").value;
+var b=document.getElementById("n2").value;
+var c=document.getElementById("n3").value;
+var d=document.getElementById("n4").value;
+
+if(a==""||b==""||c==""||d=="")
+{
+    alert("All Fields are mendatory")
+    return false;
+} 
+
+else if(b.length<10||b.length>10)
+{
+    alert("Number should be of 10 digits : Please enter valid number")
+    return false;
+} 
+else if(isNaN(b))
+{
+    alert("Only Numbers are allowed ! Please enter valid number")
+    return false;
+} 
+else if(c!=d)
+{
+    alert("Please enter same password");
+    return false;
+} 
+else
+{
+    true;
+
+}
+
+}
+
 /*Nav Bar Js*/
 function about(){
     document.getElementById("about").style.color="rgb(20,253,30)";
