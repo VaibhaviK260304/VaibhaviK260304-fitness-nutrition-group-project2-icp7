9c2bd95c5cd2e518698caa157861b1da123975b8
suraj-003 <suraj.timune2003@gmai.com>
2024-04-14 01:16:23 +0530

    changes added

--- js/guide.js
+++ js/guide.js
@@ -18,25 +18,110 @@
 
 // }
 
-function WorkoutPlan(age) {
+// function WorkoutPlan(age) {
     
-    const age=document.getElementById("age").value;
-    const Weight=document.getElementById("weight").value;
-    const height=document.getElementById("height").value;
-    const FitnessLevel=document.getElementById("FitnessLevel");
+//     const age=document.getElementById("age").value;
+//     const Weight=document.getElementById("weight").value;
+//     const height=document.getElementById("height").value;
+//     const FitnessLevel=document.getElementById("FitnessLevel");
 
 
 
-    // Check if age is between 20 and 30
-    if (age >= 20 && age <= 30)
-     {
-        const Recommendation=document.getElementById("Recommendation");
-        if (Weight>=30 && Weight<=50) {
-            const Recommendation=document.getElementById("Recommendation")
-        }
+//     // Check if age is between 20 and 30
+//     if (age >= 20 && age <= 30)
+//      {
+//         const Recommendation=document.getElementById("Recommendation");
+//         if (Weight>=30 && Weight<=50) {
+//             const Recommendation=document.getElementById("Recommendation")
+//         }
 
-    }
+//     }
 
 
+
+// }
+
+function more() {
+    
+    document.getElementById("more").style.background="yellow";
+    document.getElementById("btn-2").style.background="white";
+    document.getElementById("btn-3").style.background="white";
+    document.getElementById("btn-4").style.background="white";
 
 }
+
+function btn2() {
+    
+    document.getElementById("more").style.background="white";
+    document.getElementById("btn-2").style.background="yellow";
+    document.getElementById("btn-3").style.background="white";
+    document.getElementById("btn-4").style.background="white";
+
+}
+
+
+function btn3() {
+    
+    document.getElementById("more").style.background="whitw";
+    document.getElementById("btn-2").style.background="white";
+    document.getElementById("btn-3").style.background="yellow";
+    document.getElementById("btn-4").style.background="white";
+
+}
+
+function btn4() {
+    
+    document.getElementById("more").style.background="white";
+    document.getElementById("btn-2").style.background="white";
+    document.getElementById("btn-3").style.background="white";
+    document.getElementById("btn-4").style.background="yellow";
+
+}
+
+function btnn1() {
+    document.getElementById("btnn1").style.background="yellow";
+    document.getElementById("card").style.border="5px solid black";
+    // document.getElementById("card").style.background="lightcoral";
+    document.getElementById("Level1").style.background="lightcoral";
+    document.getElementById("btnn4").style.background="none";
+    document.getElementById("card2").style.border="none";
+    document.getElementById("Level2").style.background="none";
+    document.getElementById("btnn7").style.background="none";
+    document.getElementById("card3").style.border="none";
+    document.getElementById("Level3").style.background="none";
+
+
+    
+
+}
+function btnn2() {
+    document.getElementById("btnn4").style.background="yellow";
+    document.getElementById("card2").style.border="5 px solid black";
+    document.getElementById("Level2").style.background="lightgreen";
+    document.getElementById("btnn1").style.background="none";
+    document.getElementById("card").style.border="none";
+    // document.getElementById("card").style.background="lightcoral";
+    document.getElementById("Level1").style.background="none";
+    document.getElementById("btnn7").style.background="none";
+    document.getElementById("card3").style.border="none";
+    document.getElementById("Level3").style.background="none";
+}
+
+
+
+
+
+
+function btnn3() {
+    document.getElementById("btnn7").style.background="yellow";
+    document.getElementById("card3").style.border="5px solid black";
+    document.getElementById("Level3").style.background="lightblue";
+    document.getElementById("btnn4").style.background="none";
+    document.getElementById("card2").style.border="none";
+    document.getElementById("Level2").style.background="none";
+    document.getElementById("btnn1").style.background="none";
+    document.getElementById("card").style.border="none";
+    // document.getElementById("card").style.background="lightcoral";
+    document.getElementById("Level1").style.background="none";
+}
+
